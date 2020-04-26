17c4893a5bed09ca0051e04bca2e77e49cd2b35a
Jmacro <jonmacrohon@gmail.com>
2020-04-27 03:49:27 +1000

    list of selectors

---
+++ src/UTILS/selectors.ts
@@ -0,0 +1,19 @@
+// INPUTS
+export const searchShare: HTMLInputElement = document.querySelector<HTMLInputElement>('#searchShare')!;
+// export const nameOfShare: HTMLInputElement = document.querySelector<HTMLInputElement>('#nameOfShare')!;
+export const numberOfShares: HTMLInputElement = document.querySelector<HTMLInputElement>('#numberOfShares')!;
+export const purchasePrice: HTMLInputElement = document.querySelector<HTMLInputElement>('#purchasePrice')!;
+export const brokerage: HTMLInputElement = document.querySelector<HTMLInputElement>('#brokerage')!;
+export const targetPrice: HTMLInputElement = document.querySelector<HTMLInputElement>('#targetPrice')!;
+
+// BUTTONS
+export const closeSearch: HTMLElement = document.querySelector<HTMLElement>('#closeSearch')!;
+export const calculateButton: HTMLButtonElement = document.querySelector<HTMLButtonElement>('#calculateButton')!;
+
+// DISPLAYS
+export const searchHistory: HTMLDivElement = document.querySelector<HTMLDivElement>('#searchHistory')!;
+export const tableBodySearchHistory: HTMLTableSectionElement = document.querySelector<HTMLTableSectionElement>('#tableBodySearchHistory')!;
+export const shareDetails: HTMLDivElement = document.querySelector<HTMLDivElement>('#shareDetails')!;
+export const tableShareDetails: HTMLTableSectionElement = document.querySelector<HTMLTableSectionElement>('#tableShareDetails')!;
+export const tableBodyCalculate: HTMLTableSectionElement = document.querySelector<HTMLTableSectionElement>('#tableBodyCalculate')!;
+
