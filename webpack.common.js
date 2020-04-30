4b8c3b5720e064bbd7a8ba8c35cd6d6fc11fee27
Jmacro <jonmacrohon@gmail.com>
2020-04-30 22:38:53 +1000

    base webpack config

---
+++ webpack.common.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const plugins = [
+	new CleanWebpackPlugin(),
+	new HtmlWebpackPlugin({
+		title: 'In The Green',
+		filename: 'index.html',
+		template: './index.ejs',
+		inject: false
+	}),
+];
+
+module.exports = {
+	entry: {
+		main: './src/index.ts',
+		materialize: './src/ASSETS/js/materialize.js'
+	},
+	plugins,
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+
+	},
+	module: {
+		rules: [{
+			test: /\.tsx?$/,
+			exclude: '/node_modules/',
+			use: 'ts-loader',
+		},
+		{
+			test: /\.css$/i,
+			exclude: '/node_modules/',
+			use: ['style-loader', 'css-loader'],
+		},
+		{
+			test: /\.scss$/i,
+			exclude: '/node_modules/',
+			use: ['style-loader', 'css-loader', 'sass-loader'],
+		},
+		{
+			test: /\.exec\.js$/,
+			use: ['script-loader']
+		}],
+	},
+	// devServer: {
+	// 	contentBase: path.join(__dirname, 'dist'),
+	// 	compress: true,
+	// 	https: true,
+	// 	port: 3000,
+	// 	proxy: {
+	// 		'/api': {
+	// 			target: 'https://localhost:8080',
+	// 			secure: false,
+	// 		},
+	// 	},
+	// 	historyApiFallback: true,
+	// },
+	resolve: {
+		extensions: ['.ts', '.js', '.json']
+	},
+};
