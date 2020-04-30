ee7b003b80c58f955b772f1186491708f113324e
Jmacro <jonmacrohon@gmail.com>
2020-04-30 22:56:07 +1000

    create simple server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(__dirname + '../dist'));
+
+app.get('*', (_req, res) => {
+	res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+});
+
+app.listen(PORT, () => {
+	console.log(`App listening on PORT ${PORT}`);
+});
