abf36a17cd6cb033770d1fee45f8ce9fa294805c
Jmacr0 <jonmacrohon@gmail.com>
2020-05-10 00:22:39 +1000

    add new func to index

--- src/calculate/index.ts
+++ src/calculate/index.ts
@@ -1,9 +1,13 @@
+import { getNumberOfShares as sharesTotal } from './getNumberOfShares';
 import { getOriginalTotalValue as baseTotal } from './getOriginalTotalValue';
 import { getTargetTotalValue as targetTotal } from './getTargetTotalValue';
 import { getDifference as difference } from './getDifference';
+import * as functions from './functions';
 
 export {
+	sharesTotal,
 	baseTotal,
 	targetTotal,
-	difference
+	difference,
+	functions,
 };
